8db0a0758ee14eea84300d28e7543249871aec1f
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-04-09 09:15:44 +0000

    java practice

--- java/test.java
+++ java/test.java
@@ -1,5 +1,14 @@
 public class test{
    public static void main(String[] args){
      System.out.println("Hello,world!");
+     System.out.println("Hello");
+
+     int number = 10;
+     System.out.println(number);
+
+     int number2 = 5;
+     System.out.println(number-number2*2);
+     
+     
    }
 }
--- java/test2.java
+++ java/test2.java
@@ -1,8 +1,23 @@
-public class test{
-   public static void main(String[] args){
-     
-     int A = puts
-     
-     System.out.println("Hello,world!");
-   }
+//package day1;
+
+public class test2 {
+    /*
+     * Javaを使った最も基本的なプログラム
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello,World."); //  「Hello,World.」という文字列を表示します。
+        System.out.print("123");//改行なし
+        System.out.println("456");//改行あり
+        System.out.print(1 + "+" + "2" + "=");
+        System.out.println(1+2);
+        System.out.print(3 + "-" + 2 + "=");
+        System.out.println(3-2);
+        System.out.print(4 + "*" + 4 +"=");
+        System.out.println(4*4);
+        System.out.print(6 + "/" + 2 + "=");
+        System.out.println(6/2);
+        System.out.print(9 + "%" + 2 +"=");
+        System.out.println(9/2 + "あまり" + 9%2);
+    }
+
 }
