3bdc5148db6d17183d544846f84a92b570322c56
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-04-11 10:33:04 +0000

    java if

---
+++ java/test3.java
@@ -0,0 +1,18 @@
+public class test3{
+   public static void main(String[] args){
+     int num1 = 3;
+     int num2 = 9;
+     System.out.println(num2 - num1);
+     num1 = 5;
+     num2 = 3;
+     System.out.println((double)num1/num2);
+     double a = 7.5;
+     double b = 2.5;
+     System.out.println((int)a/(int)b);
+     final int c = 100;
+     String s = "ようこそ";
+     System.out.println(c-50);
+
+
+   }
+}
---
+++ java/test4.java
@@ -0,0 +1,14 @@
+public class test4{
+  public static void main(String[] args) {
+
+    int a = 3;
+    System.out.println("a="+a);
+    if (a<3){
+      System.out.println("aは3より小さいです");
+    }else if(a>3){
+      System.out.println("aは3より小さいです");
+    }else{
+      System.out.println("aは3です");
+    }
+  }
+}
