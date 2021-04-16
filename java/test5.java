fc15143781af8b1a583658f13fb8457cb0630a0a
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-04-16 10:13:25 +0000

    test5

--- java/test5.java
+++ java/test5.java
@@ -1,5 +1,14 @@
 public class test5{
   public static void main(String[] args){
+
+
+    for(int i = 1; i <= 5;i++){
+      System.out.print(i+" ");
+    }
+    System.out.println();
     
+    for(int i = 1; i <= 5;i++){
+      for(int j = 0: j <= 12)
+    }
   }
 }
